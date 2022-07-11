f12476b1d0604b5ca2551897deae551b1c07fbf5
coffeemiller <dev2freedom@gmail.com>
2022-07-11 13:23:30 +0900

    readme markdown doc adding

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
+
+const { width:SCREEN_WIDTH } = Dimensions.get("window");
+// const SCREEN_WIDTH = Dimensions.get("window").width;
+
+console.log(SCREEN_WIDTH);
 
 export default function App() {
   return (
+    // <View style={{ flexDirection:"row" }}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <View style={{ flex:1, backgroundColor:"tomato" }}></View>
+      <View style={{ flex:5, backgroundColor:"teal" }}></View> */}
+      <View style={styles.city}>
+        <View>
+          <Text style={styles.cityName}>Seoul</Text>
+        </View>
+      </View>
+      <ScrollView 
+        pagingEnabled 
+        horizontal 
+        showsHorizontalScrollIndicator={false}
+        // indicatorStyle="white"  // only iOS
+        contentContainerStyle={styles.weather}>
+      {/* <ScrollView style={styles.weather}> */}
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +48,67 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "orange",
+  },
+  city: {
+    flex: 1.1,
+    backgroundColor: "orange",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 50,
+    fontWeight: "700",
+  },
+  weather: {
+    // flex: 3,
+    // backgroundColor: "teal",
+  },
+  day: {
+    // flex: 1,
+    // justifyContent: "center",
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+    // backgroundColor: "teal",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
+
+
+
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       {/* <Text>Coffeemiller Expo app!!</Text> */}
+//       <Text style={styles.text}>Coffeemiller Expo app!</Text>
+//       <Button 
+//         title='버튼'
+//         onPress={() => Alert.alert("확인")}
+//       />
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   text: {
+//     fontSize:28,
+//     color:"red"
+//   }
+// });
---
+++ readme.md
@@ -0,0 +1,4 @@
+### React Native App. Edu.
+
++ React Native For Android & iOS
++ Weather Application
